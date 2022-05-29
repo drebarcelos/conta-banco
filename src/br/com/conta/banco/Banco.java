c48e62df3e81aa65183b99f5e60fc8754be93fcd
Andressa Barcelos <andressa.barcelos26@gmail.com>
2022-05-28 22:45:43 -0300

    Adiciona funcionalidade get opcao digitada

--- src/br/com/conta/banco/Banco.java
+++ src/br/com/conta/banco/Banco.java
@@ -3,9 +3,11 @@ package br.com.conta.banco;
 import java.util.Scanner;
 
 public class Banco {
+	private static final Scanner SCANNER = new Scanner(System.in);
 	
 	public void iniciar() {
 		imprimeMenu();
+		int opcao = getOpcaoDigitada();
 	}
 	
 	private void imprimeMenu() {
@@ -15,5 +17,9 @@ public class Banco {
 		System.out.println("Digite (3) para transferir");
 		System.out.println("Digite (4) para depositar");
 		System.out.println("Digite (5) para excluir sua conta");
+	}
+	
+	private int getOpcaoDigitada() {
+		return SCANNER.nextInt();
 	}
 }
