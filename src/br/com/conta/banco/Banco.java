de4320acad0859df428dbaed36f95433690ccd06
Andressa Barcelos <andressa.barcelos26@gmail.com>
2022-05-28 22:42:43 -0300

    Adiciona funcionalidade imprime menu

--- src/br/com/conta/banco/Banco.java
+++ src/br/com/conta/banco/Banco.java
@@ -1,8 +1,19 @@
 package br.com.conta.banco;
 
+import java.util.Scanner;
+
 public class Banco {
 	
 	public void iniciar() {
-		System.out.println("Olá Mundo!");
+		imprimeMenu();
+	}
+	
+	private void imprimeMenu() {
+		System.out.println("\nDigite (0) para sair do MENU");
+		System.out.println("Digite (1) para criar uma conta");
+		System.out.println("Digite (2) para visualizar seu saldo");
+		System.out.println("Digite (3) para transferir");
+		System.out.println("Digite (4) para depositar");
+		System.out.println("Digite (5) para excluir sua conta");
 	}
 }
